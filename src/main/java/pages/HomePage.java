c8d494f91c5dbc2b62d81746789ec9120ed6726d
CiprianDragu <dragucipriangabriel@gmail.com>
2024-03-11 12:11:23 +0200

    Trim a little bit

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,21 +1,12 @@
 package pages;
 
 import data.TestData;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.util.List;
-
 public class HomePage extends BasePage {
-    // Prima data declaram un camp de tip Webdriver pentru al putea folosi la PageFactory
 
-
-    // Pentru a putea declara expresia PageFactory avem nevoie de un constructor
-
-
-    // Anotarea @FindBy nu functioneaza fara PageFactory
     @FindBy(css = ".login_logo")
     private WebElement pageLogo;
     @FindBy(css = "#user-name")
